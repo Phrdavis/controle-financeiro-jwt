7ce11ab4623c8f8ea4b679d05ddfc7da5f4b9ccb
Davi Pinheiro de Souza <105741181+Phrdavis@users.noreply.github.com>
2024-11-07 16:18:24 +0000

    first commit

---
+++ src/main/java/br/com/davipinheirodesouza/controle_financeiro_jwt/ControleFinanceiroJwtApplication.java
@@ -0,0 +1,13 @@
+package br.com.davipinheirodesouza.controle_financeiro_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFinanceiroJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleFinanceiroJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/davipinheirodesouza/controle_financeiro_jwt/ControleFinanceiroJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.davipinheirodesouza.controle_financeiro_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinanceiroJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
