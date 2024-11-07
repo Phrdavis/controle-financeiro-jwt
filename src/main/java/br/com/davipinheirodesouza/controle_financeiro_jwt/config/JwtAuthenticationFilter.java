578d362d2f4386ff44b54a1cf378bb87caec0b22
Davi Pinheiro de Souza <105741181+Phrdavis@users.noreply.github.com>
2024-11-07 19:47:13 +0000

    Filtro JWT

--- src/main/java/br/com/davipinheirodesouza/controle_financeiro_jwt/config/JwtAuthenticationFilter.java
+++ src/main/java/br/com/davipinheirodesouza/controle_financeiro_jwt/config/JwtAuthenticationFilter.java
@@ -13,27 +13,4 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         this.jwtUtil = jwtUtil;
 
     }
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, javax.servlet.http.HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-
-        String token = getTokenFromRequest(request);
-
-        if (token != null && jwtUtil.validateToken(token, jwtUtil.extractUsername(token))) {
-            
-            UsernamePasswordAuthenticationToken authentication = 
-                    new UsernamePasswordAuthenticationToken(jwtUtil.extractUsername(token), null, new ArrayList<>());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        }
-
-        filterChain.doFilter(request, response);
-    }
-
-    private String getTokenFromRequest(HttpServletRequest request) {
-        String token = request.getHeader("Authorization");
-        if (token != null && token.startsWith("Bearer ")) {
-            return token.substring(7); 
-        }
-        return null;
-    }
 }
